f3973749c18188cf227f6843ed5d4b26010f6f56
gaoYingjie <1397617037@qq.com>
2022-02-14 01:07:00 +0800

    update .gitignore

--- docs/README.md
+++
@@ -1,6 +0,0 @@
----
-home: true
-
-heroText: 学习记录
-actionText: 快速踩坑 →
-actionLink: /guide/android/
--- secrets.js
+++
@@ -1,2 +0,0 @@
-const ACCESS_TOKEN = "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"
-module.exports  = ACCESS_TOKEN
